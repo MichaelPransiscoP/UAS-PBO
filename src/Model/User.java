20ea47247ef80f3c22facd3fcf4a13fd65133b5a
rapmonz <rapmonz@pendjolwang>
2022-11-18 13:12:06 +0700

    Initial commit

---
+++ src/Model/User.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+
+public class User {
+    private int userId;
+    private String userName;
+    private String userEmail;
+    private String userGender;
+    CategoryUser userCategory;
+    private int userFollowers;
+}
